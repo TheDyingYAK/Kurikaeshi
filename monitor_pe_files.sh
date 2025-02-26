08787c008c9184691de53b242e6bbd1b1b1b97df
Christopher Bowers <christopher.bowers@usmc.mil>
2025-02-26 11:14:30 -0500

    updated to md5 hash

--- monitor_pe_files.sh
+++ monitor_pe_files.sh
@@ -5,7 +5,7 @@ LOG_FILE="/var/log/pe_hashes.log"
 
 inotifywait -m -r -e close_write --format "%w%f" "$WATCH_DIR" | while read FILE; do
     if file "$FILE" | grep -q "PE32"; then
-        HASH=$(sha256sum "$FILE")
+        HASH=$(md5sum "$FILE")
         echo "$(date) - $HASH" >> "$LOG_FILE"
     fi
 done
